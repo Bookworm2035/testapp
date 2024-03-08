29b3b94c035b5c68ca1b09038ca5ed3179421c76
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-03-07 21:54:30 -0700

    yo it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import yourData from './CollegeBasketballTeams.json';
 
-function App() {
+const teamNames = yourData.teams;
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  name: string;
+  school: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+// const teamNames = [];
+
+function Welcome() {
+  return <h1>Welcome to my Basketball Teams Site </h1>;
+}
+class Team extends React.Component<TeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div>
+        {/* <img></img> */}
+        <h2>{school}</h2>
+        <h4>Mascot: {name}</h4>
+        <h4>
+          {city}, {state}
+        </h4>
+        <br></br>
+      </div>
+    );
+  }
+}
+function TeamList() {
+  // { teams }: { teams: TeamProps[] }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Team {...teamNames[0]}/> */}
+      {/* {teams.map((oneTeam) => ( */}
+      {teamNames.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
     </div>
   );
 }
 
+function App() {
+  // const [teams, setTeams] = useState<TeamProps[]>([]);
+  // useEffect(() => {
+  //   fetchTeams();
+  // }, []);
+
+  // const fetchTeams = async () => {
+  //   try {
+  //     const response = await fetch('/CollegeBasketballTeams.json');
+  //     const data = await response.json();
+  //     setTeams(data.teams);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
+  return (
+    <div className="App">
+      <Welcome />
+      <br></br>
+      <TeamList />
+      {/* teams={teams} */}
+    </div>
+  );
+}
 export default App;
